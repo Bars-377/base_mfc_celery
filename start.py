62679b5c7cab0796d8c9ad59916d4344344aea19
Неверов Вячеслав Вячеславович <neverov@mfc.tomsk.ru>
2024-12-12 14:57:57 +0700

    dfgfgd

--- start.py
+++ start.py
@@ -15,7 +15,11 @@ if __name__ == "__main__":
     t2.start()
     time.sleep(2)
 
-    t3 = threading.Thread(target=run_command, args=(['gunicorn', '--access-logfile', '-', '--error-logfile', '-', '-w', '10', '-k', 'eventlet', '-b', '0.0.0.0:5000', 'app:app'],))
+    # t3 = threading.Thread(target=run_command, args=(['gunicorn', '--access-logfile', '-', '--error-logfile', '-', '-w', '10', '-k', 'eventlet', '-b', '0.0.0.0:5000', 'app:app'],))
+    # t3.start()
+    # time.sleep(2)
+
+    t3 = threading.Thread(target=run_command, args=(['python', 'app:app'],))
     t3.start()
     time.sleep(2)
 
