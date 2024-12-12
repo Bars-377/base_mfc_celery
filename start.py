fcc6ec46da3f428552e948c789af1045e802e01a
Неверов Вячеслав Вячеславович <neverov@mfc.tomsk.ru>
2024-12-12 10:31:49 +0700

    dsffds

--- start.py
+++ start.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     t2.start()
     time.sleep(2)
 
-    t3 = threading.Thread(target=run_command, args=(["gunicorn", "--access-logfile", "-", "--error-logfile", "-", "-w", "10", "-k", "eventlet", "-b", "0.0.0.0:5000", "app:app"],))
+    t3 = threading.Thread(target=run_command, args=(["gunicorn", "--access-logfile", "-", "--error-logfile", "-", "-w", "10", "-k", "eventlet", "-b", "0.0.0.0:5000", "app:app", "--env", "FLASK_SECRET_KEY=B5FYETKek4TxfdOUzR_pG1VIfwhRS_OM_wruhP5kOrY"],))
     t3.start()
     time.sleep(2)
 
